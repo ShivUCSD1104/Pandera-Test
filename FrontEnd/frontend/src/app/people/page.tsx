2418922a3dc2d24b436786285974cd0acd91c9b0
AstuteFern <svirk@ucsd.edu>
2025-02-23 15:09:01 -0500

    test

--- FrontEnd/frontend/src/app/people/page.tsx
+++ FrontEnd/frontend/src/app/people/page.tsx
@@ -6,7 +6,7 @@ export default function MyNotePage() {
       <main className="min-h-screen bg-[url(/paper.jpg)] p-8">
       <div className="bg-white p-4 rounded-2xl shadow-[8px_8px_16px_#bebebe] hover:shadow-inner hover:shadow-gray-300">
         <div className="flex flex-col items-center">
-          <div className="text-2xl text-black">Shiv Mehta - Founder & President</div>
+          <div className="text-2xl text-black">Shiv Mehta - Co-Founder & President</div>
           <div className="text-xl text-black">{"Data Science @ UCSD '27"}</div>
           <br />
           <div className='text-sm text-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis excepturi error consequuntur nam, necessitatibus deserunt similique ipsa rem dolorem doloribus!</div>
@@ -17,6 +17,19 @@ export default function MyNotePage() {
             </a>
             <a href="#" className="flex-1 text-black p-2 pl-4 pr-4 m-6 bg-white rounded-lg hover:shadow-inner hover:shadow-gray-300 transition-all duration-300 text-center">
               Personal Website
+            </a>
+            <a href="#" className="flex-1 text-black p-2 pl-4 pr-6 m-4 bg-white rounded-lg hover:shadow-inner hover:shadow-gray-300 transition-all duration-300 text-center">
+              LinkedIn
+            </a>
+          </div>
+          <div className="text-2xl text-black">Sukhman Virk Co-Founder</div>
+          <div className="text-xl text-black">{"Mathematics - Computer Science @ UCSD '24"}</div>
+          <br />
+          <div className='text-sm text-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis excepturi error consequuntur nam, necessitatibus deserunt similique ipsa rem dolorem doloribus!</div>
+          <br />
+          <div className="flex flex-row w-full items-center bg-black rounded-lg">
+            <a href="#" className="flex-1 text-black p-2 pl-4 pr-4 m-6 bg-white rounded-lg hover:shadow-inner hover:shadow-gray-300 transition-all duration-300 text-center">
+              GitHub
             </a>
             <a href="#" className="flex-1 text-black p-2 pl-4 pr-6 m-4 bg-white rounded-lg hover:shadow-inner hover:shadow-gray-300 transition-all duration-300 text-center">
               LinkedIn
